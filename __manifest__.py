72628026f1e78dd01e1a6dc3f96591be97087453
SandaruwanB <sandarusbandara110@gmail.com>
2025-09-05 12:23:58 +0530

    [INIT] addon with configuration

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'RFQ Confirmation Levels',
+    'version': '1.0',
+    'description': 'RFQ confirmation levels (L1, L2, L3 and L4) based on total amount.',
+    'author': 'SandaruwanB',
+    'website': 'https://sandaruwanb.github.io/',
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'depends': [
+        'base',
+        'sale',
+    ],
+    'data': [
+    ],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+}
